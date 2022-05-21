57d216f10b8f72d5120e412ca320c32ced4cee9f
jmholzer <jannic.holzer@gmail.com>
2022-05-21 06:58:02 +0200

    Add initial implementation of Reader class.

---
+++ csv_parser.py
@@ -0,0 +1,74 @@
+from typing import Iterable
+from enum import Enum, auto
+
+
+class RowStates(Enum):
+    UNQUOTED_FIELD = auto()
+    QUOTED_FIELD = auto()
+    QUOTED_QUOTE = auto()
+
+
+class Reader:
+    """
+    
+    """
+
+    def __init__(self, csv_file: Iterable[str]):
+        """
+        """
+        self.csv_file = csv_file
+        self._init_header()
+
+    def _init_header(self) -> None:
+        """
+        """
+        try:
+            first_lane = next(self.csv_file)
+            self._header = self._parse_line(first_lane)
+        except StopIteration:
+            pass
+
+    def __iter__(self):
+        """
+        """
+        return self
+    
+    def __next__(self):
+        """
+        """
+        while True:
+            line = next(self.csv_file).strip()
+            line_values = self._parse_line(line)
+            if len(line_values) == len(self._header):
+                break
+        
+        return dict(zip(self._header, line_values))
+        
+    def _parse_line(self, line: str) -> None:
+        """
+        """
+        values = [""]
+        state = RowStates.UNQUOTED_FIELD
+        for c in line:
+            if state == RowStates.UNQUOTED_FIELD:
+                if c == ',':
+                    values.append("")
+                elif c == '"':
+                    state = RowStates.QUOTED_FIELD
+                else:
+                    values[-1] += c
+            elif state == RowStates.QUOTED_FIELD:
+                if c == '"':
+                    state = RowStates.QUOTED_QUOTE
+                else:
+                    values[-1] += c
+            elif state == RowStates.QUOTED_QUOTE:
+                if c == ',':
+                    values.append("")
+                    state = RowStates.UNQUOTED_FIELD
+                elif c == '"':
+                    values[-1] += '"'
+                    state = RowStates.QUOTED_FIELD
+                else:
+                    state = RowStates.UNQUOTED_FIELD
+        return values
