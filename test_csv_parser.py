f0e317c69735c6d7a057b905e013f242a949b344
jmholzer <jannic.holzer@gmail.com>
2022-05-21 08:26:22 +0200

    Add unit tests for csv_parser module.

---
+++ test_csv_parser.py
@@ -0,0 +1,23 @@
+import unittest
+from csv_parser import Reader
+
+
+class TestReader(unittest.TestCase):
+    """
+    
+    """
+
+    def test_init_header(self):
+        """
+        """
+        pass
+
+    def test_next(self):
+        pass
+
+    def test_parse_line(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
