d742a38eb9a46aacce7b0177048d20a5c19ad022
Jannic Holzer <jannic.holzer@quantumblack.com>
2023-04-09 21:38:05 +0100

    Add complete test suite

--- test/test_csv_reader.py
+++ test/test_csv_reader.py
@@ -1,21 +1,28 @@
 import unittest
-from csv_parser import Reader
+from csv_fsm import Reader
 
 
 class TestReader(unittest.TestCase):
     """
-    
+    Tests for the Reader class defined in csv_reader.py.
     """
+
     def setUp(self) -> None:
-        self.test_file = open("resources/test.csv", "r")
+        """
+        Open a file with test data before each test is run.
+        """
+        self.test_file = open("test_csv_parser/test.csv", "r")
         self.reader = Reader(self.test_file)
 
     def tearDown(self) -> None:
+        """
+        Close the open test date file after each test is run.
+        """
         self.test_file.close()
 
     def test_empty_file(self) -> None:
         """
-        
+        Assert that giving an empty file as input raises an exception.
         """
         with open("test_csv_parser/empty.csv", "r") as empty_file:
             with self.assertRaises(Exception):
@@ -23,38 +30,37 @@ class TestReader(unittest.TestCase):
 
     def test_init_header(self) -> None:
         """
-
+        Assert that header initialisation is performed correctly.
         """
-        expected = ['header1','header2','header,3','"header4""']
+        expected = ["header1", "header2", "header,3", '"header4""']
         result = self.reader._header
         self.assertEqual(expected, result)
 
     def test_next(self) -> None:
         """
-        
+        Assert that the next valid line in the input file is parsed.
         """
         expected = {
             "header1": "value1",
             "header2": "value2",
             "header,3": "value3",
-            "\"header4\"\"": "value4"
+            '"header4""': "value4",
         }
         result = next(self.reader)
         self.assertEqual(expected, result)
 
     def test_parse_line(self) -> None:
         """
-        
+        Assert that CSV lines are being parsed correctly.
         """
         expected = [
-            ["value1","value2","value3","value4"],
-            ["value5","value6","value,7","\"value8\""],
+            ["value1", "value2", "value3", "value4"],
+            ["value5", "value6", "value,7", '"value8"'],
             [""],
-            ["value9","","",""],
-            ["value10","value11","value12","value 14"] 
+            ["value9", "", "", ""],
+            ["value10", "value11", "value12", "value 14"],
         ]
         result = [self.reader._parse_line(line.strip()) for line in self.test_file]
-        pprint(result)
         self.assertEqual(expected, result)
 
 
